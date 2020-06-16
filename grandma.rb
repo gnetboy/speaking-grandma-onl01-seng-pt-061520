ea16c80fbfa1c4f6f075b611db33fbe5cf832421
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-16 00:20:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 
 def speak_to_grandma(string)
 
-  if string==string.upcase
+  if string==string.downcase
   
-    return "NO, NOT SINCE 1938!"
+    return 
   
   elsif string == string.downcase
     return "HUH?! SPEAK UP, SONNY!"
